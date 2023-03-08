048cbc1487f652e4e25d536eb12115e171ee0e74
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-03-08 14:38:25 +0200

    BG COLOR / REVEAL SCRIPT

---
+++ JS/REVEAL.js
@@ -0,0 +1,16 @@
+function reveal() {
+      let reveals = document.querySelectorAll(".reveal");
+      for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+    }
+    window.addEventListener("scroll", reveal);
+    
+
