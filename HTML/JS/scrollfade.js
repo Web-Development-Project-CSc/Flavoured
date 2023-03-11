c605f7a77d986d7d70aaca94d883928a66da714a
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-03-11 19:01:38 +0200

    PICS

--- HTML/JS/scrollfade.js
+++ HTML/JS/scrollfade.js
@@ -1,30 +1,30 @@
-var fadeElements = document.getElementsByClassName('scrollfade');
+let fadeElements = document.getElementsByClassName('scrollfade');
 
 function scrollFade() {
-	var viewportBottom = window.scrollY + window.innerHeight;
+    let viewportBottom = window.scrollY + window.innerHeight;
 
-	for (var index = 0; index < fadeElements.length; index++) {
-		var element = fadeElements[index];
-		var rect = element.getBoundingClientRect();
+    for (let index = 0; index < fadeElements.length; index++) {
+        let element = fadeElements[index];
+        let rect = element.getBoundingClientRect();
 
-		var elementFourth = rect.height/4;
-		var fadeInPoint = window.innerHeight - elementFourth;
-		var fadeOutPoint = -(rect.height/2);
+        let elementFourth = rect.height/4;
+        let fadeInPoint = window.innerHeight - elementFourth;
+        let fadeOutPoint = -(rect.height/2);
 
-		if (rect.top <= fadeInPoint) {
-			element.classList.add('--visible');
-			element.classList.add('--animate');
-			element.classList.remove('--hidden');
-		} else {
-			element.classList.remove('--visible');
-			element.classList.add('--hidden');
-		}
+        if (rect.top <= fadeInPoint) {
+            element.classList.add('--visible');
+            element.classList.add('--animate');
+            element.classList.remove('--hidden');
+        } else {
+            element.classList.remove('--visible');
+            element.classList.add('--hidden');
+        }
 
-		if (rect.top <= fadeOutPoint) {
-			element.classList.remove('--visible');
-			element.classList.add('--hidden');
-		}
-	}
+        if (rect.top <= fadeOutPoint) {
+            element.classList.remove('--visible');
+            element.classList.add('--hidden');
+        }
+    }
 }
 
 document.addEventListener('scroll', scrollFade);
@@ -32,3 +32,4 @@ window.addEventListener('resize', scrollFade);
 document.addEventListener('DOMContentLoaded', function() {
     scrollFade();
 });
+
