0db285c2f8e72ec3396100d659c70f5a1a6ea6e3
AbdulRahman Sherif <abdelrahmansherif630@gmail.com>
2023-03-11 22:16:24 +0200

    VALIDATION

--- HTML/JS/verification.js
+++ HTML/JS/verification.js
@@ -1,5 +1,4 @@
-  
-    function confirmpass(){
+ function confirmpass(){
         let p=document.getElementById("pword").value;
         let pc=document.getElementById("cpword").value;
         let x= document.getElementById("cc");
@@ -11,3 +10,13 @@
           x.classList.remove("show");
           }  
     }
+function validateAge()
+{
+let b=document.getElementById("bdate").value;
+let x = new Date();
+if(new Date(b).getTime()<= x.getTime()){
+  alert("nooo");
+  return false;
+}
+return true;
+}
