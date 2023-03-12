d9f44b4215b3becb7c1120fb32e86f251cf861a2
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-03-12 13:10:55 +0200

    SUPPORT

--- HTML/JS/infofade.js
+++ HTML/JS/infofade.js
@@ -1,13 +1,16 @@
-let index = 0;
-infoFade();
+let index = 0, p = document.getElementsByClassName("paragraph");
+document.getElementById("info").addEventListener(
+  "mouseenter",
+  (event) => {
+   infoFade();
+  },
+);
 function infoFade(){
     let i;
-    let p = document.getElementsByClassName("paragraph");
     for (i = 0; i < p.length; i++) {
-        p[i].style.display = "none";  
+      p[i].style.display = "none";  
       }
       index++;
       if (index > p.length) {index = 1}    
       p[index-1].style.display = "block"; 
- setTimeout(infoFade, 5000); 
 }
